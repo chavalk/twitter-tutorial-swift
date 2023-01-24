8526a196655ad84db3d1e518df2dcd903e57ed1d
chavalk <chavalk@hotmail.com>
2023-01-23 23:31:47 -0600

    Add main tab controller.

---
+++ TwitterTutorial/Controller/MainTabController.swift
@@ -0,0 +1,17 @@
+//
+//  MainTabController.swift
+//  TwitterTutorial
+//
+//  Created by Jose Garcia on 1/23/23.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemPink
+    }
+}
