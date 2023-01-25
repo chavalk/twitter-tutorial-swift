cdf16e8168439684244f4973d6694061355e6985
chavalk <chavalk@hotmail.com>
2023-01-24 21:29:32 -0600

    Add marks.

--- TwitterTutorial/Controller/MainTabController.swift
+++ TwitterTutorial/Controller/MainTabController.swift
@@ -9,9 +9,15 @@ import UIKit
 
 class MainTabController: UITabBarController {
 
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .systemPink
     }
+    
+    // MARK: - Helpers
 }
