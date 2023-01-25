35da622289e894dcefce486b9de0fb60fa62924e
chavalk <chavalk@hotmail.com>
2023-01-24 21:37:39 -0600

    Add explore controller.

---
+++ TwitterTutorial/Controller/ExploreController.swift
@@ -0,0 +1,21 @@
+//
+//  ExploreController.swift
+//  TwitterTutorial
+//
+//  Created by Jose Garcia on 1/24/23.
+//
+
+import UIKit
+
+class ExploreController: UIViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - Helpers
+}
