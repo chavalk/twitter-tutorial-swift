aa0477275e86fb97a6cfb87de1b217588276f6a4
chavalk <chavalk@hotmail.com>
2023-01-24 21:40:19 -0600

    Add conversations controller.

---
+++ TwitterTutorial/Controller/ConversationsController.swift
@@ -0,0 +1,21 @@
+//
+//  ConversationsController.swift
+//  TwitterTutorial
+//
+//  Created by Jose Garcia on 1/24/23.
+//
+
+import UIKit
+
+class ConversationsController: UIViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - Helpers
+}
