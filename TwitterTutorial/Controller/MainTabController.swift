ab7620d437f1b0e03c4334ab4338d6bd02454ba2
chavalk <chavalk@hotmail.com>
2023-01-25 16:27:45 -0600

    Use extension to add button constraints.

--- TwitterTutorial/Controller/MainTabController.swift
+++ TwitterTutorial/Controller/MainTabController.swift
@@ -32,11 +32,7 @@ class MainTabController: UITabBarController {
     
     func configureUI() {
         view.addSubview(actionButton)
-        actionButton.translatesAutoresizingMaskIntoConstraints = false
-        actionButton.heightAnchor.constraint(equalToConstant: 56).isActive = true
-        actionButton.widthAnchor.constraint(equalToConstant: 56).isActive = true
-        actionButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -64).isActive = true
-        actionButton.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -16).isActive = true
+        actionButton.anchor(bottom: view.safeAreaLayoutGuide.bottomAnchor, right: view.rightAnchor, paddingBottom: 64, paddingRight: 16, width: 56, height: 56)
         actionButton.layer.cornerRadius = 56 / 2
     }
     
