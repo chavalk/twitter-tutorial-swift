67b074e33fbd3ff340ed7d985f6d3eee78343206
chavalk <chavalk@hotmail.com>
2023-01-25 17:14:39 -0600

    Add login controller.

---
+++ TwitterTutorial/Controller/Authentication/LoginController.swift
@@ -0,0 +1,27 @@
+//
+//  File.swift
+//  TwitterTutorial
+//
+//  Created by Jose Garcia on 1/25/23.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+    // MARK: - Selectors
+    
+    // MARK: - Helpers
+    
+    func configureUI() {
+        view.backgroundColor = .twitterBlue
+    }
+}
