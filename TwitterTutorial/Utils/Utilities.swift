207f1983059173a60a035fbaa76722b5dc1aa302
chavalk <chavalk@hotmail.com>
2023-01-25 18:58:23 -0600

    Add text field parameter back to input container view and add text field to view.

--- TwitterTutorial/Utils/Utilities.swift
+++ TwitterTutorial/Utils/Utilities.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class Utilities {
     
-    func inputContainerView(withImage image: UIImage) -> UIView {
+    func inputContainerView(withImage image: UIImage, textField: UITextField) -> UIView {
         let view = UIView()
         let iv = UIImageView()
         view.heightAnchor.constraint(equalToConstant: 50).isActive = true
@@ -19,6 +19,9 @@ class Utilities {
         view.addSubview(iv)
         iv.anchor(left: view.leftAnchor, bottom: view.bottomAnchor, paddingLeft: 8, paddingBottom: 8)
         iv.setDimensions(width: 24, height: 24)
+        
+        view.addSubview(textField)
+        textField.anchor(left: iv.rightAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, paddingLeft: 8, paddingBottom: 8)
         
         return view
     }
