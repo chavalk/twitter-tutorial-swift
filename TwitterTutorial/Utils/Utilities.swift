6e54fd7fe2e1c163f721a4584f242b6554deafce
chavalk <chavalk@hotmail.com>
2023-01-25 18:36:58 -0600

    Add input container view function.

--- TwitterTutorial/Utils/Utilities.swift
+++ TwitterTutorial/Utils/Utilities.swift
@@ -9,4 +9,17 @@ import UIKit
 
 class Utilities {
     
+    func inputContainerView(withImage image: UIImage, textField: UITextField) -> UIView {
+        let view = UIView()
+        let iv = UIImageView()
+        view.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+        iv.image = image
+        
+        view.addSubview(iv)
+        iv.anchor(left: view.leftAnchor, bottom: view.bottomAnchor, paddingLeft: 8, paddingBottom: 8)
+        iv.setDimensions(width: 24, height: 24)
+        
+        return view
+    }
 }
