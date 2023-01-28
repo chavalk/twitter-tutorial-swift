eb7300edf15a54c14de2e5e97f6952f76b97428a
chavalk <chavalk@hotmail.com>
2023-01-27 23:12:48 -0600

    Move code making calls to database from registration controller to auth service file.

--- TwitterTutorial/API/AuthService.swift
+++ TwitterTutorial/API/AuthService.swift
@@ -11,6 +11,37 @@ struct AuthService {
     static let shared = AuthService()
     
     func registerUser() {
+        guard let imageData = profileImage.jpegData(compressionQuality: 0.3) else { return }
+        let fileName = NSUUID().uuidString
+        let storageRef = STORAGE_PROFILE_IMAGES.child(fileName)
         
+        storageRef.putData(imageData, metadata: nil) { meta, error in
+            if let error = error {
+                print("DEBUG: Error is \(error.localizedDescription)")
+                return
+            }
+            storageRef.downloadURL { url, error in
+                if let error = error {
+                    print("DEBUG: Error is \(error.localizedDescription)")
+                    return
+                }
+                guard let profileImageUrl = url?.absoluteString else { return }
+                
+                Auth.auth().createUser(withEmail: email, password: password) { result, error in
+                    if let error = error {
+                        print("DEBUG: Error is \(error.localizedDescription)")
+                        return
+                    }
+                    
+                    guard let uid = result?.user.uid else { return }
+                    
+                    let values = ["email": email, "username": username, "fullName": fullName, "profileImageUrl": profileImageUrl]
+                    
+                    REF_USERS.child(uid).updateChildValues(values) { (error, ref) in
+                        print("DEBUG: Sucessfully updated user information")
+                    }
+                }
+            }
+        }
     }
 }
--- TwitterTutorial/Controller/Authentication/RegistrationController.swift
+++ TwitterTutorial/Controller/Authentication/RegistrationController.swift
@@ -108,39 +108,8 @@ class RegistrationController: UIViewController {
         guard let fullName = fullNameTextField.text else { return }
         guard let username = usernameTextField.text else { return }
         
-        guard let imageData = profileImage.jpegData(compressionQuality: 0.3) else { return }
-        let fileName = NSUUID().uuidString
-        let storageRef = STORAGE_PROFILE_IMAGES.child(fileName)
+        AuthService.shared.registerUser()
         
-        
-        storageRef.putData(imageData, metadata: nil) { meta, error in
-            if let error = error {
-                print("DEBUG: Error is \(error.localizedDescription)")
-                return
-            }
-            storageRef.downloadURL { url, error in
-                if let error = error {
-                    print("DEBUG: Error is \(error.localizedDescription)")
-                    return
-                }
-                guard let profileImageUrl = url?.absoluteString else { return }
-                
-                Auth.auth().createUser(withEmail: email, password: password) { result, error in
-                    if let error = error {
-                        print("DEBUG: Error is \(error.localizedDescription)")
-                        return
-                    }
-                    
-                    guard let uid = result?.user.uid else { return }
-                    
-                    let values = ["email": email, "username": username, "fullName": fullName, "profileImageUrl": profileImageUrl]
-                    
-                    REF_USERS.child(uid).updateChildValues(values) { (error, ref) in
-                        print("DEBUG: Sucessfully updated user information")
-                    }
-                }
-            }
-        }
     }
     
     @objc func handleShowLogIn() {
