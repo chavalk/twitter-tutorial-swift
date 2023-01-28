b4ffd7d2e61901bf30a876686bfb55cc9c3e322f
chavalk <chavalk@hotmail.com>
2023-01-27 23:08:20 -0600

    Add auth service file.

---
+++ TwitterTutorial/API/AuthService.swift
@@ -0,0 +1,16 @@
+//
+//  AuthService.swift
+//  TwitterTutorial
+//
+//  Created by Jose Garcia on 1/27/23.
+//
+
+import Foundation
+
+struct AuthService {
+    static let shared = AuthService()
+    
+    func registerUser() {
+        
+    }
+}
