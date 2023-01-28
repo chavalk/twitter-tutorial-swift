a7eb8b00bc0007db04256446949969d0f766761e
chavalk <chavalk@hotmail.com>
2023-01-28 12:21:30 -0600

    Add user service file.

---
+++ TwitterTutorial/API/UserService.swift
@@ -0,0 +1,16 @@
+//
+//  UserService.swift
+//  TwitterTutorial
+//
+//  Created by Jose Garcia on 1/28/23.
+//
+
+import Foundation
+
+struct UserService {
+    static let shared = UserService()
+    
+    func fetchUser() {
+        
+    }
+}
