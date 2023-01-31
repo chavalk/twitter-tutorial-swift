ac281aabd6bcc3718e738fa8ea68e4b4d4678e49
chavalk <chavalk@hotmail.com>
2023-01-30 21:08:41 -0600

    Add upload tweet controller.

---
+++ TwitterTutorial/Controller/UploadTweetController.swift
@@ -0,0 +1,27 @@
+//
+//  UploadTweetController.swift
+//  TwitterTutorial
+//
+//  Created by Jose Garcia on 1/30/23.
+//
+
+import UIKit
+
+class UploadTweetController: UIViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+    
+    // MARK: - Selectors
+    
+    // MARK: - API
+    
+    // MARK: - Helpers
+}
