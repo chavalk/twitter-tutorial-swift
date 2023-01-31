8d3304452efc9455bf0a0d223518252348016104
chavalk <chavalk@hotmail.com>
2023-01-30 21:48:26 -0600

    Add configure UI function.

--- TwitterTutorial/Controller/UploadTweetController.swift
+++ TwitterTutorial/Controller/UploadTweetController.swift
@@ -15,13 +15,24 @@ class UploadTweetController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        view.backgroundColor = .red
+        configureUI()
     }
     
     // MARK: - Selectors
     
+    @objc func handleCancel() {
+        dismiss(animated: true, completion: nil)
+    }
+    
     // MARK: - API
     
     // MARK: - Helpers
+    
+    func configureUI() {
+        view.backgroundColor = .white
+        navigationController?.navigationBar.barTintColor = .white
+//        navigationController?.navigationBar.isTranslucent = false
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(handleCancel))
+    }
 }
