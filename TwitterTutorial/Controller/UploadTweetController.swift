cb0403bfd7f0582735e6cabf5e75366a4db03287
chavalk <chavalk@hotmail.com>
2023-01-30 22:08:47 -0600

    Add action button.

--- TwitterTutorial/Controller/UploadTweetController.swift
+++ TwitterTutorial/Controller/UploadTweetController.swift
@@ -11,6 +11,22 @@ class UploadTweetController: UIViewController {
     
     // MARK: - Properties
     
+    private lazy var actionButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.backgroundColor = .twitterBlue
+        button.setTitle("Tweet", for: .normal)
+        button.titleLabel?.textAlignment = .center
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        button.setTitleColor(.white, for: .normal)
+        
+        button.frame = CGRect(x: 0, y: 0, width: 64, height: 32)
+        button.layer.cornerRadius = 32 / 2
+        
+        button.addTarget(self, action: #selector(handleUploadTweet), for: .touchUpInside)
+        
+        return button
+    }()
+    
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
@@ -24,6 +40,10 @@ class UploadTweetController: UIViewController {
         dismiss(animated: true, completion: nil)
     }
     
+    @objc func handleUploadTweet() {
+        print("DEBUG: Upload tweet here...")
+    }
+    
     // MARK: - API
     
     // MARK: - Helpers
@@ -34,5 +54,6 @@ class UploadTweetController: UIViewController {
 //        navigationController?.navigationBar.isTranslucent = false
         
         navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(handleCancel))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(customView: actionButton)
     }
 }
