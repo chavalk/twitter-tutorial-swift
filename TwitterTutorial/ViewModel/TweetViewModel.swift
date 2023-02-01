7b8b9bedc8eefb029fcbdacb7fa7c19cf4ed0429
chavalk <chavalk@hotmail.com>
2023-02-01 17:53:37 -0600

    Add tweet view model.

---
+++ TwitterTutorial/ViewModel/TweetViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  TweetViewModel.swift
+//  TwitterTutorial
+//
+//  Created by Jose Garcia on 2/1/23.
+//
+
+import Foundation
+
+struct TweetViewModel {
+    
+    let tweet: Tweet
+    
+    init(tweet: Tweet) {
+        self.tweet = tweet
+    }
+}
