ba471564fd22b137fefa7df87f4374d2556a4348
chavalk <chavalk@hotmail.com>
2023-02-01 14:13:49 -0600

    Add tweet cell.

---
+++ TwitterTutorial/View/TweetCell.swift
@@ -0,0 +1,21 @@
+//
+//  TweetCell.swift
+//  TwitterTutorial
+//
+//  Created by Jose Garcia on 2/1/23.
+//
+
+import UIKit
+
+class TweetCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .red
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
