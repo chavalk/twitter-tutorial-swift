353e582176b04e1bb833780d0cd5422fdf0d544c
chavalk <chavalk@hotmail.com>
2023-01-31 22:46:42 -0600

    Add call to database in tweet service.

--- TwitterTutorial/API/TweetService.swift
+++ TwitterTutorial/API/TweetService.swift
@@ -10,9 +10,11 @@ import Firebase
 struct TweetService {
     static let shared = TweetService()
     
-    func uploadTweet(caption: String, completion: @escaping(DatabaseReference, Error?) -> Void) {
+    func uploadTweet(caption: String, completion: @escaping(Error?, DatabaseReference) -> Void) {
         guard let uid = Auth.auth().currentUser?.uid else { return }
         
         let values = ["uid": uid, "timestamp": Int(NSDate().timeIntervalSince1970), "likes": 0, "retweets": 0, "caption": caption] as [String : Any]
+        
+        REF_TWEETS.childByAutoId().updateChildValues(values, withCompletionBlock: completion)
     }
 }
