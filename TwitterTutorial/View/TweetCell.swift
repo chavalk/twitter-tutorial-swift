2e0312acf5a05d129e7bd62f6dd8497614a17ce2
chavalk <chavalk@hotmail.com>
2023-02-01 17:03:38 -0600

    Add var to store tweet and configure function.

--- TwitterTutorial/View/TweetCell.swift
+++ TwitterTutorial/View/TweetCell.swift
@@ -11,6 +11,10 @@ class TweetCell: UICollectionViewCell {
     
     // MARK: - Properties
     
+    var tweet: Tweet? {
+        didSet { configure() }
+    }
+    
     private let profileImageView: UIImageView = {
         let iv = UIImageView()
         iv.contentMode = .scaleAspectFit
@@ -128,5 +132,7 @@ class TweetCell: UICollectionViewCell {
     
     // MARK: - Helpers
     
-    
+    func configure() {
+        
+    }
 }
