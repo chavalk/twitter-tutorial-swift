d4e924d78fcf595d7590b310228928e9f6618a48
chavalk <chavalk@hotmail.com>
2023-01-31 22:18:46 -0600

    Add observer to hide placeholder when user starts typing and make text color black for typed text.

--- TwitterTutorial/View/CaptionTextView.swift
+++ TwitterTutorial/View/CaptionTextView.swift
@@ -25,15 +25,24 @@ class CaptionTextView: UITextView {
         super.init(frame: frame, textContainer: textContainer)
         
         backgroundColor = .white
+        textColor = .black
         font = UIFont.systemFont(ofSize: 16)
         isScrollEnabled = false
         heightAnchor.constraint(equalToConstant: 300).isActive = true
         
         addSubview(placeholderLabel)
         placeholderLabel.anchor(top: topAnchor, left: leftAnchor, paddingTop: 8, paddingLeft: 4)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(handleTextInputChange), name: UITextView.textDidChangeNotification, object: nil)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Selectors
+    
+    @objc func handleTextInputChange() {
+        placeholderLabel.isHidden = !text.isEmpty
     }
 }
