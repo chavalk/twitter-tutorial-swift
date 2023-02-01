1df2ae203a38b66ae2f6d3bf3bcc8c3753b1b99b
chavalk <chavalk@hotmail.com>
2023-02-01 15:13:42 -0600

    Add marks and change background color to white.

--- TwitterTutorial/View/TweetCell.swift
+++ TwitterTutorial/View/TweetCell.swift
@@ -9,13 +9,23 @@ import UIKit
 
 class TweetCell: UICollectionViewCell {
     
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        backgroundColor = .red
+        backgroundColor = .white
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    // MARK: - Selectors
+    
+    // MARK: - Helpers
+    
+    
 }
