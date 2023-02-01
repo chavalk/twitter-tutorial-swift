9f115a4dc26ba677167d17aea86828f6e334ba4f
chavalk <chavalk@hotmail.com>
2023-01-31 21:53:08 -0600

    Add more properties to caption text view.

--- TwitterTutorial/View/CaptionTextView.swift
+++ TwitterTutorial/View/CaptionTextView.swift
@@ -23,6 +23,11 @@ class CaptionTextView: UITextView {
     override init(frame: CGRect, textContainer: NSTextContainer?) {
         super.init(frame: frame, textContainer: textContainer)
         
+        backgroundColor = .white
+        font = UIFont.systemFont(ofSize: 16)
+        isScrollEnabled = false
+        heightAnchor.constraint(equalToConstant: 300).isActive = true
+        
         addSubview(placeholderLabel)
         placeholderLabel.anchor(top: topAnchor, left: leftAnchor, paddingTop: 8, paddingLeft: 4)
     }
