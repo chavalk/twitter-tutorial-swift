060f5017e103cb05604c40f42049889a4a45bdad
chavalk <chavalk@hotmail.com>
2023-02-01 17:38:58 -0600

    Configure fetch tweets function to fetch user along with tweets.

--- TwitterTutorial/API/TweetService.swift
+++ TwitterTutorial/API/TweetService.swift
@@ -26,7 +26,11 @@ struct TweetService {
             guard let uid = dictionary["uid"] as? String else { return }
             let tweetID = snapshot.key
             
-            UserService.shared.fetchUser(completion: <#T##(User) -> Void#>)
+            UserService.shared.fetchUser(uid: uid) { user in
+                let tweet = Tweet(user: user, tweetID: tweetID, dictionary: dictionary)
+                tweets.append(tweet)
+                completion(tweets)
+            }
         }
     }
 }
