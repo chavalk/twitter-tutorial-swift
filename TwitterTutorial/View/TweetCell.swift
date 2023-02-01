8958edc7080c14f42b81eea998006dfcab3737bc
chavalk <chavalk@hotmail.com>
2023-02-01 15:30:57 -0600

    Add profile image view, caption label, and info label.

--- TwitterTutorial/View/TweetCell.swift
+++ TwitterTutorial/View/TweetCell.swift
@@ -11,12 +11,34 @@ class TweetCell: UICollectionViewCell {
     
     // MARK: - Properties
     
+    private let profileImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFit
+        iv.clipsToBounds = true
+        iv.setDimensions(width: 48, height: 48)
+        iv.layer.cornerRadius = 48 / 2
+        iv.backgroundColor = .twitterBlue
+        return iv
+    }()
+    
+    private let captionLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    private let infoLabel = UILabel()
+    
     // MARK: - Lifecycle
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         
         backgroundColor = .white
+        
+        addSubview(profileImageView)
+        profileImageView.anchor(top: topAnchor, left: leftAnchor, paddingTop: 12, paddingLeft: 8)
     }
     
     required init?(coder: NSCoder) {
