fa073b88bde5b3ba99f15860061353f5ef97052f
chavalk <chavalk@hotmail.com>
2023-01-31 21:40:55 -0600

    Add caption text view.

---
+++ TwitterTutorial/View/CaptionTextView.swift
@@ -0,0 +1,33 @@
+//
+//  CaptionTextView.swift
+//  TwitterTutorial
+//
+//  Created by Jose Garcia on 1/31/23.
+//
+
+import UIKit
+
+class CaptionTextView: UITextView {
+    
+    // MARK: - Properties
+    
+    let placeholderLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.textColor = .darkGray
+        return label
+    }()
+    
+    // MARK: - Lifecycle
+    
+    override init(frame: CGRect, textContainer: NSTextContainer?) {
+        super.init(frame: frame, textContainer: textContainer)
+        
+        addSubview(placeholderLabel)
+        placeholderLabel.anchor(top: topAnchor, left: leftAnchor, paddingTop: 8, paddingLeft: 4)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
