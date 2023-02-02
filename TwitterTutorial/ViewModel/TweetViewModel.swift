a05b77131ab588fb09421ebf7de92fb857f36025
chavalk <chavalk@hotmail.com>
2023-02-01 18:15:28 -0600

    Add user info text in tweet view model.

--- TwitterTutorial/ViewModel/TweetViewModel.swift
+++ TwitterTutorial/ViewModel/TweetViewModel.swift
@@ -5,17 +5,27 @@
 //  Created by Jose Garcia on 2/1/23.
 //
 
-import Foundation
+import UIKit
 
 struct TweetViewModel {
     
     let tweet: Tweet
+    let user: User
     
     var profileImageUrl: URL? {
         return tweet.user.profileImageUrl
     }
     
+    var userInfoText: NSAttributedString {
+        let title = NSMutableAttributedString(string: user.fullName, attributes: [.font: UIFont.boldSystemFont(ofSize: 14)])
+        
+        title.append(NSAttributedString(string: " @\(user.username)", attributes: [.font: UIFont.systemFont(ofSize: 14), .foregroundColor: UIColor.lightGray]))
+        
+        return title
+    }
+    
     init(tweet: Tweet) {
         self.tweet = tweet
+        self.user = tweet.user
     }
 }
