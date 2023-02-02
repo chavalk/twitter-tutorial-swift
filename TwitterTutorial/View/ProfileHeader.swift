7f2331d4df259174bbb1dd5581082c8aa7486ca0
chavalk <chavalk@hotmail.com>
2023-02-02 17:12:15 -0600

    Add container view and back button.

--- TwitterTutorial/View/ProfileHeader.swift
+++ TwitterTutorial/View/ProfileHeader.swift
@@ -11,15 +11,40 @@ class ProfileHeader: UICollectionReusableView {
     
     // MARK: - Properties
     
+    private lazy var containerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .twitterBlue
+        
+        view.addSubview(backButton)
+        backButton.anchor(top: view.topAnchor, left: view.leftAnchor, paddingTop: 42, paddingLeft: 16)
+        backButton.setDimensions(width: 30, height: 30)
+        
+        return view
+    }()
+    
+    private lazy var backButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(named: "baseline_arrow_back_white_24dp"), for: .normal)
+        button.addTarget(self, action: #selector(handleDismissal), for: .touchUpInside)
+        return button
+    }()
+    
     // MARK: - Lifecycle
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        backgroundColor = .red
+        addSubview(containerView)
+        containerView.anchor(top: topAnchor, left: leftAnchor, right: rightAnchor, height: 108)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Selectors
+    
+    @objc func handleDismissal() {
+        
     }
 }
