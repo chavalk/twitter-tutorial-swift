d139caa0283a714b6738d55387fa19596d788224
chavalk <chavalk@hotmail.com>
2023-02-01 20:28:42 -0600

    Add profile controller.

---
+++ TwitterTutorial/Controller/ProfileController.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileController.swift
+//  TwitterTutorial
+//
+//  Created by Jose Garcia on 2/1/23.
+//
+
+import UIKit
+
+class ProfileController: UICollectionViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView.backgroundColor = .red
+    }
+}
