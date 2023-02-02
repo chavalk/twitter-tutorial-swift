4325fcf79938da9c71a19e42f34b80107372c064
chavalk <chavalk@hotmail.com>
2023-02-01 22:28:37 -0600

    Add extension to dequeue header.

--- TwitterTutorial/Controller/ProfileController.swift
+++ TwitterTutorial/Controller/ProfileController.swift
@@ -49,6 +49,15 @@ extension ProfileController {
     }
 }
 
+// MARK: - UICollectionViewDelegate
+
+extension ProfileController {
+    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: headerIdentifier, for: indexPath) as! ProfileHeader
+        return header
+    }
+}
+
 // MARK: - UICollectionViewDelegateFlowLayout
 
 extension ProfileController: UICollectionViewDelegateFlowLayout {
