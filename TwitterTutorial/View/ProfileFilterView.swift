a2b90b833f6821fbc7fe72bfdd04cc9bf484c650
chavalk <chavalk@hotmail.com>
2023-02-03 17:03:21 -0600

    Add profile filter view.

---
+++ TwitterTutorial/View/ProfileFilterView.swift
@@ -0,0 +1,31 @@
+//
+//  ProfileFilterView.swift
+//  TwitterTutorial
+//
+//  Created by Jose Garcia on 2/3/23.
+//
+
+import UIKit
+
+class ProfileFilterView: UIView {
+    
+    // MARK: - Properties
+    
+    lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.backgroundColor = .white
+        return cv
+    }()
+    
+    // MARK: - Lifecycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .red
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
