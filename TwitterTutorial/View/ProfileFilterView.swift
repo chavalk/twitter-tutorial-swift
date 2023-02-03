dfb64ba5befb2d826511d3d201f519e3b63f651f
chavalk <chavalk@hotmail.com>
2023-02-03 17:53:43 -0600

    Add delegate, datasource, and register cell.

--- TwitterTutorial/View/ProfileFilterView.swift
+++ TwitterTutorial/View/ProfileFilterView.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 
+private let reuseIdentifier = "ProfileFilterCell"
+
 class ProfileFilterView: UIView {
     
     // MARK: - Properties
@@ -15,6 +17,8 @@ class ProfileFilterView: UIView {
         let layout = UICollectionViewFlowLayout()
         let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
         cv.backgroundColor = .white
+        cv.delegate = self
+        cv.dataSource = self
         return cv
     }()
     
@@ -22,10 +26,28 @@ class ProfileFilterView: UIView {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        
+        collectionView.register(ProfileFilterCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+        
         backgroundColor = .red
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+}
+
+extension ProfileFilterView: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! ProfileFilterCell
+        return cell
+    }
+}
+
+extension ProfileFilterView: UICollectionViewDelegate {
+    
 }
