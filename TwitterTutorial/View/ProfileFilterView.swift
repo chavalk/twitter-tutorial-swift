3b23db7bdfe76369e435431ee5385fdcbf2b1fc7
chavalk <chavalk@hotmail.com>
2023-02-03 18:03:32 -0600

    Add function for spacing between cells.

--- TwitterTutorial/View/ProfileFilterView.swift
+++ TwitterTutorial/View/ProfileFilterView.swift
@@ -63,4 +63,8 @@ extension ProfileFilterView: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: frame.width / 3, height: frame.height)
     }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
 }
