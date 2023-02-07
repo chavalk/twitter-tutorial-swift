45c88c8a104fd860ca41bf19ae181ac0f460736e
chavalk <chavalk@hotmail.com>
2023-02-06 18:25:37 -0600

    Update tweet cell delegate extension.

--- TwitterTutorial/Controller/FeedController.swift
+++ TwitterTutorial/Controller/FeedController.swift
@@ -96,10 +96,9 @@ extension FeedController: UICollectionViewDelegateFlowLayout {
 // MARK: - TweetCellDelegate
 
 extension FeedController: TweetCellDelegate {
-    func handleProfileImageTapped() {
+    func handleProfileImageTapped(_ cell: TweetCell) {
+        guard let user = cell.tweet?.user else { return }
         let controller = ProfileController(user: user)
         navigationController?.pushViewController(controller, animated: true)
     }
-    
-    
 }
