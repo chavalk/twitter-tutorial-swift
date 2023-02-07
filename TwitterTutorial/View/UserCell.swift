7d4c11962bb54e4ad7f967fd931d26d3019882a8
chavalk <chavalk@hotmail.com>
2023-02-07 17:24:56 -0600

    Add profile image view.

--- TwitterTutorial/View/UserCell.swift
+++ TwitterTutorial/View/UserCell.swift
@@ -11,12 +11,23 @@ class UserCell: UITableViewCell {
     
     // MARK: - Properties
     
+    private lazy var profileImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFit
+        iv.clipsToBounds = true
+        iv.setDimensions(width: 32, height: 32)
+        iv.layer.cornerRadius = 32 / 2
+        iv.backgroundColor = .twitterBlue
+        return iv
+    }()
+    
     // MARK: - Lifecycle
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
-        backgroundColor = .systemPurple
+        addSubview(profileImageView)
+        profileImageView.centerY(inView: self, leftAnchor: leftAnchor, paddingLeft: 12)
     }
     
     required init?(coder: NSCoder) {
