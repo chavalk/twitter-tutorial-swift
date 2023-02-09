3ce0e08b630cde29f00556846c7a20efeb2ef513
chavalk <chavalk@hotmail.com>
2023-02-08 21:34:56 -0600

    Add tweet header file.

---
+++ TwitterTutorial/View/TweetHeader.swift
@@ -0,0 +1,25 @@
+//
+//  TweetHeader.swift
+//  TwitterTutorial
+//
+//  Created by Jose Garcia on 2/8/23.
+//
+
+import UIKit
+
+class TweetHeader: UICollectionReusableView {
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .systemPurple
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
