f7c86fe7cd7e930f56f60a3c6fad53523e0ded7d
chavalk <chavalk@hotmail.com>
2023-02-08 23:08:41 -0600

    Add retweets label and likes label.

--- TwitterTutorial/View/TweetHeader.swift
+++ TwitterTutorial/View/TweetHeader.swift
@@ -67,6 +67,18 @@ class TweetHeader: UICollectionReusableView {
         return button
     }()
     
+    private lazy var retweetsLabel: UILabel = {
+        let label = UILabel()
+        label.text = "2 Retweets"
+        return label
+    }()
+    
+    private lazy var likesLabel: UILabel = {
+        let label = UILabel()
+        label.text = "0 Likes"
+        return label
+    }()
+    
     private lazy var statsView: UIView = {
         let view = UIView()
         view.backgroundColor = .red
