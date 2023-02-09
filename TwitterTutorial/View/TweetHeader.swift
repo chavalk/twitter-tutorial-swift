e56a234f725cbb6eb98f9b3b6758dc2a4b40ce34
chavalk <chavalk@hotmail.com>
2023-02-08 22:14:56 -0600

    Add profile image view and its selector function.

--- TwitterTutorial/View/TweetHeader.swift
+++ TwitterTutorial/View/TweetHeader.swift
@@ -11,6 +11,21 @@ class TweetHeader: UICollectionReusableView {
     
     // MARK: - Properties
     
+    private lazy var profileImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFit
+        iv.clipsToBounds = true
+        iv.setDimensions(width: 48, height: 48)
+        iv.layer.cornerRadius = 48 / 2
+        iv.backgroundColor = .twitterBlue
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handleProfileImageTapped))
+        iv.addGestureRecognizer(tap)
+        iv.isUserInteractionEnabled = true
+        
+        return iv
+    }()
+    
     // MARK: - Lifecycle
     
     override init(frame: CGRect) {
@@ -21,5 +36,11 @@ class TweetHeader: UICollectionReusableView {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Selectors
+    
+    @objc func handleProfileImageTapped() {
+        
     }
 }
