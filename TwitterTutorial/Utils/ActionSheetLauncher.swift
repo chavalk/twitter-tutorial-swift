bcece1b5c3fd2daddbae7dfe039ac9f9e91ca921
chavalk <chavalk@hotmail.com>
2023-02-11 15:11:16 -0600

    Add action sheet launcher file.

---
+++ TwitterTutorial/Utils/ActionSheetLauncher.swift
@@ -0,0 +1,20 @@
+//
+//  ActionSheetLauncher.swift
+//  TwitterTutorial
+//
+//  Created by Jose Garcia on 2/11/23.
+//
+
+import Foundation
+
+class ActionSheetLauncher: NSObject {
+    
+    // MARK: - Properties
+    
+    private let user: User
+    
+    init(user: User) {
+        self.user = user
+        super.init()
+    }
+}
