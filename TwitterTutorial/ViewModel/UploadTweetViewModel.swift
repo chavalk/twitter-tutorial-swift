30640f3865e617870ceaf9612405750f586446d0
chavalk <chavalk@hotmail.com>
2023-02-10 21:07:00 -0600

    Add upload tweet view model.

---
+++ TwitterTutorial/ViewModel/UploadTweetViewModel.swift
@@ -0,0 +1,24 @@
+//
+//  UploadTweetViewModel.swift
+//  TwitterTutorial
+//
+//  Created by Jose Garcia on 2/10/23.
+//
+
+import UIKit
+
+struct UploadTweetViewModel {
+    
+    let actionButtonTitle: String
+    let placeholderText: String
+    var shouldShorReplyLabel: Bool
+    
+    init(config: UploadTweetConfiguration) {
+        switch config {
+        case .tweet:
+            <#code#>
+        case .reply(_):
+            <#code#>
+        }
+    }
+}
