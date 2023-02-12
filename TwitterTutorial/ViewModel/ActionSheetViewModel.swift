aba992ee0f29f2b4aa02b7a55d27695a3b3f7d11
chavalk <chavalk@hotmail.com>
2023-02-12 16:35:27 -0600

    Add action sheet options enum.

--- TwitterTutorial/ViewModel/ActionSheetViewModel.swift
+++ TwitterTutorial/ViewModel/ActionSheetViewModel.swift
@@ -15,3 +15,19 @@ struct ActionSheetViewModel {
         self.user = user
     }
 }
+
+enum ActionSheetOptions {
+    case follow(User)
+    case unfollow(User)
+    case report
+    case delete
+    
+    var description: String {
+        switch self {
+        case .follow(let user): return "Follow @\(user.username)"
+        case .unfollow(let user): return "Unfollow @\(user.username)"
+        case .report: return "Report Tweet"
+        case .delete: return "Delete Tweet"
+        }
+    }
+}
