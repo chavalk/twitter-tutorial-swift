6354d61aee39a7fdc6e71dfa62a78405765e7a5a
chavalk <chavalk@hotmail.com>
2023-02-12 16:13:19 -0600

    Add action sheet view model

---
+++ TwitterTutorial/ViewModel/ActionSheetViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  ActionSheetViewModel.swift
+//  TwitterTutorial
+//
+//  Created by Jose Garcia on 2/12/23.
+//
+
+import Foundation
+
+struct ActionSheetViewModel {
+    
+    private let user: User
+    
+    init(user: User) {
+        self.user = user
+    }
+}
