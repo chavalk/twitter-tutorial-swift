2b80e38e6ec9893d3c53dd56fca40e63e99160aa
chavalk <chavalk@hotmail.com>
2023-02-14 21:54:46 -0600

    Configure upload notification function to add tweet ID to dictionary if tweet is passed in as parameter.

--- TwitterTutorial/API/NotificationService.swift
+++ TwitterTutorial/API/NotificationService.swift
@@ -10,9 +10,13 @@ import Firebase
 struct NotificationService {
     static let shared = NotificationService()
     
-    func uploadNotification(type: NotificationType) {
+    func uploadNotification(type: NotificationType, tweet: Tweet? = nil) {
         guard let uid = Auth.auth().currentUser?.uid else { return }
         
         var values: [String: Any] = ["timestamp": Int(NSDate().timeIntervalSince1970), "uid": uid, "type": type.rawValue]
+        
+        if let tweet = tweet {
+            values["tweetID"] = tweet.tweetID
+        }
     }
 }
