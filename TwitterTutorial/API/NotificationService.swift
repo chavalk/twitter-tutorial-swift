fbe2cb71d66d4954d9413f28a3a3ce4321f637fd
chavalk <chavalk@hotmail.com>
2023-02-14 21:40:34 -0600

    Create uid variable and values variable.

--- TwitterTutorial/API/NotificationService.swift
+++ TwitterTutorial/API/NotificationService.swift
@@ -5,12 +5,14 @@
 //  Created by Jose Garcia on 2/14/23.
 //
 
-import Foundation
+import Firebase
 
 struct NotificationService {
     static let shared = NotificationService()
     
     func uploadNotification(type: NotificationType) {
-        print("DEBUG: Type is \(type)")
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        
+        var values: [String: Any] = ["timestamp": Int(NSDate().timeIntervalSince1970), "uid": uid, "type": type.rawValue]
     }
 }
