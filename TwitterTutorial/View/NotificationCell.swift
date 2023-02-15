033ccd5dc65624b186677f1c5eeb1573868d8cfe
chavalk <chavalk@hotmail.com>
2023-02-15 11:10:37 -0600

    Add notification cell.

---
+++ TwitterTutorial/View/NotificationCell.swift
@@ -0,0 +1,23 @@
+//
+//  NotificationCell.swift
+//  TwitterTutorial
+//
+//  Created by Jose Garcia on 2/15/23.
+//
+
+import UIKit
+
+class NotificationCell: UITableViewCell {
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
