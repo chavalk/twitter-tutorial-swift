730f4d48a414d6c76c99708971973a4a22ce5cfb
chavalk <chavalk@hotmail.com>
2023-02-15 10:58:04 -0600

    Add extension to notifications controller to set number of rows and cell content in table view.

--- TwitterTutorial/Controller/NotificationsController.swift
+++ TwitterTutorial/Controller/NotificationsController.swift
@@ -26,3 +26,13 @@ class NotificationsController: UITableViewController {
         navigationItem.title = "Notifications"
     }
 }
+
+extension NotificationsController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+}
