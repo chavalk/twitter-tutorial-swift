d4ce51d9f874d29ac5fb667d761a2a105b1ba0ab
chavalk <chavalk@hotmail.com>
2023-02-14 21:17:42 -0600

    Add notification service and upload notification function.

---
+++ TwitterTutorial/API/NotificationService.swift
@@ -0,0 +1,16 @@
+//
+//  NotificationService.swift
+//  TwitterTutorial
+//
+//  Created by Jose Garcia on 2/14/23.
+//
+
+import Foundation
+
+struct NotificationService {
+    static let shared = NotificationService()
+    
+    func uploadNotification(type: NotificationType) {
+        
+    }
+}
