323adc7a475b161877be2fd7e995164c797168c0
chavalk <chavalk@hotmail.com>
2023-02-15 11:37:24 -0600

    Add profile image view property and the selector function for its gesture recognizer.

--- TwitterTutorial/View/NotificationCell.swift
+++ TwitterTutorial/View/NotificationCell.swift
@@ -11,6 +11,21 @@ class NotificationCell: UITableViewCell {
     
     // MARK: - Properties
     
+    private lazy var profileImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFit
+        iv.clipsToBounds = true
+        iv.setDimensions(width: 48, height: 48)
+        iv.layer.cornerRadius = 48 / 2
+        iv.backgroundColor = .twitterBlue
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handleProfileImageTapped))
+        iv.addGestureRecognizer(tap)
+        iv.isUserInteractionEnabled = true
+        
+        return iv
+    }()
+    
     // MARK: - Lifecycle
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
@@ -19,5 +34,11 @@ class NotificationCell: UITableViewCell {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Selector
+    
+    @objc func handleProfileImageTapped() {
+        
     }
 }
