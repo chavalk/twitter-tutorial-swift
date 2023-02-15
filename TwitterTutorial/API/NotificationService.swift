2af22562a69953af40ea19a42fd7497a4c1fe473
chavalk <chavalk@hotmail.com>
2023-02-14 22:52:15 -0600

    Add user parameter tp upload notification function and add call to database to save notification if user is passed in as parameter.

--- TwitterTutorial/API/NotificationService.swift
+++ TwitterTutorial/API/NotificationService.swift
@@ -10,7 +10,7 @@ import Firebase
 struct NotificationService {
     static let shared = NotificationService()
     
-    func uploadNotification(type: NotificationType, tweet: Tweet? = nil) {
+    func uploadNotification(type: NotificationType, tweet: Tweet? = nil, user: User? = nil) {
         guard let uid = Auth.auth().currentUser?.uid else { return }
         
         var values: [String: Any] = ["timestamp": Int(NSDate().timeIntervalSince1970), "uid": uid, "type": type.rawValue]
@@ -18,6 +18,8 @@ struct NotificationService {
         if let tweet = tweet {
             values["tweetID"] = tweet.tweetID
             REF_NOTIFICATIONS.child(tweet.user.uid).childByAutoId().updateChildValues(values)
+        } else if let user = user {
+            REF_NOTIFICATIONS.child(user.uid).childByAutoId().updateChildValues(values)
         }
     }
 }
