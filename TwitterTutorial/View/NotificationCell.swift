4b93e7b08bb609f9286f2a2588157d8b4ba3338c
chavalk <chavalk@hotmail.com>
2023-02-15 14:19:58 -0600

    Create view model and set profile image view nad notification label in configure function.

--- TwitterTutorial/View/NotificationCell.swift
+++ TwitterTutorial/View/NotificationCell.swift
@@ -64,6 +64,10 @@ class NotificationCell: UITableViewCell {
     
     // MARK: - Helpers
     func configure() {
+        guard let notification = notification else { return }
+        let viewModel = NotificationViewModel(notification: notification)
         
+        profileImageView.sd_setImage(with: viewModel.profileImageUrl)
+        notificationLabel.attributedText = viewModel.notificationText
     }
 }
