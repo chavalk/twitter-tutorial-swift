cd8283355ed3c679b5b4b8c46eb690b8f672da06
chavalk <chavalk@hotmail.com>
2023-02-15 22:26:24 -0600

    Add for loop to check if user is followed.

--- TwitterTutorial/Controller/NotificationsController.swift
+++ TwitterTutorial/Controller/NotificationsController.swift
@@ -35,6 +35,16 @@ class NotificationsController: UITableViewController {
     func fetchNotifications() {
         NotificationService.shared.fetchNotifications { notifications in
             self.notifications = notifications
+            
+            for (index, notification) in notifications.enumerated() {
+                if case .follow = notification.type {
+                    let user = notification.user
+                    
+                    UserService.shared.checkIfUserIsFollowed(uid: user.uid) { isFollowed in
+                        self.notifications[index].user.isFollowed = isFollowed
+                    }
+                }
+            }
         }
     }
     
