d32f36069f5486ef04aace5ac720424ad4947235
chavalk <chavalk@hotmail.com>
2023-02-17 16:53:54 -0600

    Add marks to tweet view model.

--- TwitterTutorial/ViewModel/TweetViewModel.swift
+++ TwitterTutorial/ViewModel/TweetViewModel.swift
@@ -9,6 +9,8 @@ import UIKit
 
 struct TweetViewModel {
     
+    // MARK: - Properties
+    
     let tweet: Tweet
     let user: User
     
@@ -62,6 +64,8 @@ struct TweetViewModel {
         return UIImage(named: imageName)!
     }
     
+    // MARK: - Lifecycle
+    
     init(tweet: Tweet) {
         self.tweet = tweet
         self.user = tweet.user
@@ -73,6 +77,8 @@ struct TweetViewModel {
         
         return attributedTitle
     }
+    
+    // MARK: - Helpers
     
     func size(forWidth width: CGFloat) -> CGSize {
         let measurementLabel = UILabel()
