f7b1c6dae3052fcfc85209db504a8797c49a2059
chavalk <chavalk@hotmail.com>
2023-02-22 15:37:35 -0600

    Add edit profile controller.

---
+++ TwitterTutorial/Controller/EditProfileController.swift
@@ -0,0 +1,25 @@
+//
+//  EditProfileController.swift
+//  TwitterTutorial
+//
+//  Created by Jose Garcia on 2/22/23.
+//
+
+import UIKit
+
+class EditProfileController: UITableViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - Selectors
+    
+    // MARK: - API
+    
+    // MARK: - Helpers
+}
