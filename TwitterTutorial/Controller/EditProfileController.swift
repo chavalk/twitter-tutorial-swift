223b96b62e15f95bc4c72ef18f954263d6946cca
chavalk <chavalk@hotmail.com>
2023-02-22 15:42:25 -0600

    Add initialization with user and user property.

--- TwitterTutorial/Controller/EditProfileController.swift
+++ TwitterTutorial/Controller/EditProfileController.swift
@@ -11,7 +11,18 @@ class EditProfileController: UITableViewController {
     
     // MARK: - Properties
     
+    private let user: User
+    
     // MARK: - Lifecycle
+    
+    init(user: User) {
+        self.user = user
+        super.init(style: .plain)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
