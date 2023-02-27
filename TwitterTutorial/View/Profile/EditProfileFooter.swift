ff640fbdad333a64edb53ed30078475bb102d347
chavalk <chavalk@hotmail.com>
2023-02-26 21:12:07 -0600

    Add edit profile footer.

---
+++ TwitterTutorial/View/Profile/EditProfileFooter.swift
@@ -0,0 +1,23 @@
+//
+//  EditProfileFooter.swift
+//  TwitterTutorial
+//
+//  Created by Jose Garcia on 2/26/23.
+//
+
+import UIKit
+
+class EditProfileFooter: UIView {
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
