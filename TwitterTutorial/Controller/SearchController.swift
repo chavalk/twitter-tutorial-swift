f918a765a0a4bcf42a3b19a641c00c6f13eb502f
chavalk <chavalk@hotmail.com>
2023-02-28 16:16:22 -0600

    Add property for config and add initialization function for it.

--- TwitterTutorial/Controller/SearchController.swift
+++ TwitterTutorial/Controller/SearchController.swift
@@ -18,6 +18,8 @@ class SearchController: UITableViewController {
     
     // MARK: - Properties
     
+    private let config: SearchControllerConfiguration
+    
     private var users = [User]() {
         didSet { tableView.reloadData() }
     }
@@ -33,6 +35,15 @@ class SearchController: UITableViewController {
     private let searchController = UISearchController(searchResultsController: nil)
     
     // MARK: - Lifecycle
+    
+    init(config: SearchControllerConfiguration) {
+        self.config = config
+        super.init(style: .plain)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
